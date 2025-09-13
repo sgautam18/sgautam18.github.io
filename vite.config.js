527b010c7aecff9760ef1b9aaea89dca8967b881
sid <gautamsiddharth1810@gmail.com>
2025-09-13 14:15:02 +0530

    new commit 3

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'docs',        // ⬅️ build straight into /docs
-    emptyOutDir: true      // clears old files before new build
+    outDir: 'docs',
+    emptyOutDir: true
   },
-  base: './'               // important for GitHub Pages relative paths
+  base: 'sgautam18.github.io',   // 👈 important for GitHub Pages
 })
